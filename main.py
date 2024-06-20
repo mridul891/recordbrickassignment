9fc32af893dde40fade0896456dd8a7ad4e8914d
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-06-20 17:49:49 +0530

    compoleted register and login routes

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import flask, request, jsonify
+from database import app, mongo
+from models import User, Login, LinkID
+from operations import create_user, authenticate_user, Link_id, delete_user
 
-app = Flask(__name__)
+user_collection = mongo.db.users
+linked_id_collection = mongo.db.linked_ids
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route('/register')
-def register_page():
-    return "<h1>Hello mridul</h1>"
+@app.routes('/register', methods=['POST'])
+def register():
+    # gets the json data from the body and  automatically gets converted  into the dictionary
+    user_data = request.json
+
+    # This is basically used to  unpack the elements in the dictionary
+    user = User(**user_data)
+
+    if user_collection.find_one({"email": user.email}):
+        # return the json object with the message and status code
+        return jsonify({"error": 'Email already Registered'}), 400
+
+    # user.dict() basically converts the code back to the dictionary
+    create_user(user.dict(), user_collection)
+    return jsonify({"message": "User Registered Successfully"}), 201
+
+
+@app.routes('/login', methods=['POST'])
+def login():
+    login_data = request.json
+
+    login = Login(**login_data)
+
+    user = authenticate_user(login.email, login.password, user_collection)
+
+    if not user:
+        return jsonify({"error": "Invalid Credentials"})
+
+    return jsonify({"message": " Loggin succesfull"})
